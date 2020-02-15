53dfc7965dcad0a8b43fbb396aadb9530466e0ef
Voltti <sainvo@utu.fi>
2020-02-15 15:39:54 +0200

    updated course app 2.0

--- Part2/courseapp2/src/index.js
+++ Part2/courseapp2/src/index.js
@@ -1,13 +1,11 @@
-/*Fullstack MOOC Part2 ex 2.1- 2.5 (EVAL)
-@author Matti Luukkainen
-@editor Sanna Volanen
+/*WEB AND MOBILE PROGRAMMING, UTU
+* Part2 ex 2.1- 2.3
+@author Sanna Volanen
 */
 /*TASKS: 
-1) Course component
-2) Total
-3) Total with reduce method
-4) Generalize to random amount of courses
-5) Course module
+1) Rendering collections, generalize to random amount of courses
+2) Total number of exercises, with reduce method
+3) Course module
 */
 
 import React from 'react';
@@ -22,99 +20,45 @@ const Courses = ({courses}) => {
             course ={unit}
         />     
         )
-//let's take one course at a time out of the courses array
-    //course component contains the rendering of all subcomponents, Header, Content and Total
-    //MAKE COURSE A SEPARATE COMPONENT
-    /*const Course = ({course}) =>{
-        const Header = ({course}) =>{ //renderöi kurssin nimen otsikoksi
-            return(
-                <div>
-                    <h2>
-                        <b><u>{course.name}</u></b>
-                    </h2>
-                </div>
-            )
-        }
-        
-        const Content = ({course}) =>{              //renderöi kurssin osat; param props on array
-            const parts = () =>            //renderöi osan nimen ja tehtävämäärä
-                course.parts.map(part =>
-                    <li key={part.id}>
-                        {part.name}, {part.exercises}
-                    </li>
-                )
-                return(
-                    <ul>{parts()}</ul>
-                )
-        }    
-    
-        const Total = ({course}) =>{ //renderöi kurssin tehtävien kokonaismäärän
-            //mapataan exercises alkiot ulos coursesta
-            const exercises = course.parts.map(part =>part.exercises)
-            console.log({exercises})
-            //iteroidaan reducella summaksi
-            var total = exercises.reduce((total, value)=>{
-                console.log(total, value)
-                return total + value
-            })
-            console.log({total})
-            //palautetaan muotoiltu Total
-            return(
-                <p><b>Total of {total} exercises</b></p>
-            )
-        }
-        return (
-            <div>
-                <Header course={course} />
-                <Content course={course}/>
-                <Total course={course}/>
-            </div>
-        )
-    }*/ 
     
     return(
     <ol>{units()}</ol>
     )   
 }
-//SCALING UP NO OF COURSES
+
 const App = () => {
     const courses = [
         {
-            name: 'Half Stack application development',
+            name: 'Superadvanced web and mobile programming',
             parts: [
                 { 
-                    name: 'Fundamentals of React',
-                    exercises: 10,
+                    name: 'Basics of React',
+                    exercises: 8,
                     id: 1 
                 },
                 {
-                    name: 'Using props to pass data',
-                    exercises: 7,
+                    name: 'Using props',
+                    exercises: 10,
                     id: 2
                 },
                 {
-                    name:'State of a component',
-                    exercises: 14,
+                    name:'Component states',
+                    exercises: 12,
                     id: 3
-                }, 
-                {
-                    name: 'Redux',
-                    exercises: 11,
-                    id: 4
                 }
             ]
         },
         {
-            name: 'Node.js',
+            name: 'TBA',
             parts:[
                 {
-                    name: 'Node.js',
-                    exercises: 3,
+                    name: 'TBA',
+                    exercises: 0,
                     id: 1
                 },
                 {
-                    name: 'Middlewares',
-                    exercises: 7,
+                    name: 'TBA',
+                    exercises: 0,
                     id: 2
                 }
             ]
@@ -122,7 +66,7 @@ const App = () => {
     ] 
     return(
         <div>
-            <h1>BASICS OF WEB DEVELOPMENT</h1>
+            <h1>Superadvanced web and mobile programming</h1>
             <h2>CURRICULUM</h2>
             <Courses courses={courses}/>
         </div>
